38df7ff058b3212021f7a55dd98201475a27679b
Ola Adeeko <ojadeeko@gmail.com>
2022-07-18 19:15:15 +0100

    created selected attribute

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+import { addItem } from "../../redux/cart/cart.actions";
 import { createStructuredSelector } from "reselect";
 import { selectCurrencySwitch } from "../../redux/currency/currency.selector";
 import './collection-item.style.scss';
@@ -12,38 +13,43 @@ class CollectionItem extends React.Component {
     
     render() {
        console.log(this.props)
-       const {currencySwitch, item} = this.props
+       const {currencySwitch, item, addItem} = this.props
        const { id, brand, name, gallery, inStock } = item
         const price = item.prices.find((price => price?.currency?.label === currencySwitch?.label));
 
         const floatPrice = parseFloat(price?.amount).toFixed(2)
        
             return (
-                <Link to={`/product/${id}`}>
-                    <div className={`${inStock ? '' : 'out-stock'} collection-item `} >
-     
-                        <div className="image-container">
-                            <img src={gallery[0]} alt={name} className='image' />
-                        </div>
-    
-                        <div className="collection-footer">
-                            <span className="brand">{brand} </span>
-                            <span className="name">{name}</span>
-                            <span className="price">{price?.currency?.symbol}{floatPrice}</span>
-                        </div>
+                
+                <div className={`${inStock ? '' : 'out-stock'} collection-item `} >
+                        <Link to={`/product/${id}`}>
+                            <div className="image-container">
+                                <img src={gallery[0]} alt={name} className='image' />
+                            </div>
+        
+                            <div className="collection-footer">
+                                <span className="brand">{brand} </span>
+                                <span className="name">{name}</span>
+                                <span className="price">{price?.currency?.symbol}{floatPrice}</span>
+                            </div>
+                        </Link> 
                         <div className="stock">
                             <span>OUT OF STOCK</span>
                         </div>
-                        <div className="cart-icon" >< CartLogo className="icon" /></div>
-                    </div>
-                </Link>   
+                        <div className="cart-icon" >< CartLogo className="icon" onClick={() => addItem(item)}/></div>
+                </div>
+                 
             );
     }
 }
-      
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item)),
+})
+
 
 const mapStateToProps = createStructuredSelector({
     currencySwitch : selectCurrencySwitch
 });
 
-    export default connect(mapStateToProps)(CollectionItem);
+export default connect(mapStateToProps, mapDispatchToProps)(CollectionItem);
--- src/components/product-item/product-item.component.jsx
+++ src/components/product-item/product-item.component.jsx
@@ -10,38 +10,78 @@ import { displayItem } from "../../redux/cart/cart.actions"
 import { createStructuredSelector } from 'reselect'
 import Attribute from "../attributes/attributes.component";
 import CustomButton from "../../components/custom-button/custom-button.component";
+import Selected from "../selected/selected.component";
+
 
 
 class Product extends React.Component {
-    constructor () {
-        super();
+    constructor (props) {
+        super(props);
 
         this.state = {
             index : 0,
             attributes: '',
-            addToBagMsg: "Select Choices To Add To Bag",
-            selected:{}
-        }
+            dot: this.props.item,
+            product: {}
 
+        }
     }
-    handleChange = (value) => {
-        console.log(value)
-        this.setState({attributes: value})
+    
+    productObject = () => {
+        const product = {
+            id : this.props.item.id,
+            name: this.props.item.name,
+            brand: this.props.item.brand,
+            category: this.props.item.category,
+            attributes: this.props.item.attributes,
+            gallery : this.props.item.gallery,
+            inStock : this.props.item.inStock,
+            description: this.props.item.description,
+            prices: this.props.item.prices,
+            selected: {}
+        }
+    
+        this.props.item.attributes.forEach((attribute) => {
+            product.selected[attribute.name] = ""
+        });
+
+        this.setState({
+            product: product
+        })
     }
 
     
+    componentWillMount() {
+        this.productObject()
+    }
+    
+    selected =(selected) => {
+        selected = {}
+    }
+    handleChange = (attributeName, value) => {
+        this.setState({
+            product: {
+                ...this.state.product,
+                selected: {
+                    ...this.state.product.selected,
+                    [attributeName]: value
+                }
+            }
+        })
+       
+    }
 
     handleTab = (index) => {
         this.setState({index:index})
     }
     render() {
-        console.log(this.props.item)
+        console.log(this.state)
         
         var parse = require('html-react-parser')
 
-        const item = this.props.item
+        const { product } = this.state
         const {currencySwitch, addItem, display} = this.props
-        const price = item.prices.find((price => price?.currency?.label === currencySwitch.label));
+        const price = product.prices.find((price => price?.currency?.label === currencySwitch.label));
         var priceFloat = parseFloat(price?.amount).toFixed(2)
        
         return (
@@ -50,9 +90,9 @@ class Product extends React.Component {
                     <div className="small-image-container">
 
                             {
-                                item.gallery.map((image, index) => (
+                                product.gallery.map((image, index) => (
                                     <div className="small-image">
-                                        <img className="small" src={image} alt='item' key={index}
+                                        <img className="small" src={image} alt='product' key={index}
                                         onClick={() => this.handleTab(index)}
                                         />
                                     </div>
@@ -63,36 +103,37 @@ class Product extends React.Component {
                     </div>
                 </div>
                 <div className="main">
-                    <div className={`${item.inStock ? '' : 'out-stock'} large-image-container`}>
-                        <img className="large" src={item.gallery[this.state.index]} onClick={() => display(item)} alt="item" />
+                    <div className={`${product.inStock ? '' : 'out-stock'} large-image-container`}>
+                        <img className="large" src={product.gallery[this.state.index]} onClick={() => display(product)} alt="product" />
                         <div className="stock">
                                 <span>OUT OF STOCK</span>
                         </div>
                     </div>
                
                     <div className="display-content">
-                        <span className="brand">{item.brand}</span>
-                        <span className="name">{item.name}</span>
+                        <span className="brand">{product.brand}</span>
+                        <span className="name">{product.name}</span>
                         <div className="attribute">
                             {
-                                item.attributes.map(attribute => (
-                                    <Attribute key={attribute.id} attribute={attribute} onClick={display} onChange={this.handleChange} />
+                                this.state.dot.attributes.map(attribute => (
+                                    <Selected key={attribute.id} attribute={attribute} selected={product.selected} onClick={display} onChange={this.handleChange} />
+
                                 ))
                             }
                         </div>
-                        
+                            
                         <span > 
                             <div className="price">PRICE:</div>
                             <div className="amount">{price?.currency?.symbol}{priceFloat}</div>
                         </span>
                         <div className="button">
                             {
-                                (item.inStock )  ? (<CustomButton onClick={() => addItem(item)}>ADD TO CART</CustomButton>) : <CustomButton outStock>ADD TO CART</CustomButton>
+                                (product.inStock )  ? (<CustomButton onClick={() => addItem(product)}>ADD TO CART</CustomButton>) : <CustomButton outStock>ADD TO CART</CustomButton>
                             }
                         </div>
                         
                         <div className="description">
-                            {parse(item.description)}
+                            {parse(product.description)}
                         </div>
                     </div>
                 
